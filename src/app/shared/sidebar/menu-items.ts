9ad34528f90f348ce981196e4060a493147f0d86
badisjl99 <badisjlassi86@gmail.com>
2025-04-30 22:21:13 +0100

    navbar updates fixes --V0.0.2

--- src/app/shared/sidebar/menu-items.ts
+++ src/app/shared/sidebar/menu-items.ts
@@ -4,7 +4,7 @@ export const ROUTES: RouteInfo[] = [
  
   {
     path: '/dashboard',
-    title: 'Dashboard',
+    title: 'Badis',
     icon: 'bi bi-speedometer2',
     class: '',
     extralink: false,
@@ -12,7 +12,7 @@ export const ROUTES: RouteInfo[] = [
   },
   {
     path: '/component/alert',
-    title: 'Alert',
+    title: 'Dridi',
     icon: 'bi bi-bell',
     class: '',
     extralink: false,
@@ -20,15 +20,25 @@ export const ROUTES: RouteInfo[] = [
   },
   {
     path: '/component/badges',
-    title: 'Badges',
+    title: 'Smiri',
     icon: 'bi bi-patch-check',
     class: '',
     extralink: false,
     submenu: []
   },
   {
+    path: '/component/dropdown',
+    title: 'AbdRahim',
+    icon: 'bi bi-menu-app',
+    class: '',
+    extralink: false,
+    submenu: []
+  },
+ 
+ 
+  {
     path: '/component/buttons',
-    title: 'Button',
+    title: 'Farah',
     icon: 'bi bi-hdd-stack',
     class: '',
     extralink: false,
@@ -36,50 +46,11 @@ export const ROUTES: RouteInfo[] = [
   },
   {
     path: '/component/card',
-    title: 'Card',
+    title: 'Feten',
     icon: 'bi bi-card-text',
     class: '',
     extralink: false,
     submenu: []
   },
-  {
-    path: '/component/dropdown',
-    title: 'Dropdown',
-    icon: 'bi bi-menu-app',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/pagination',
-    title: 'Pagination',
-    icon: 'bi bi-dice-1',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/nav',
-    title: 'Nav',
-    icon: 'bi bi-pause-btn',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/component/table',
-    title: 'Table',
-    icon: 'bi bi-layout-split',
-    class: '',
-    extralink: false,
-    submenu: []
-  },
-  {
-    path: '/about',
-    title: 'About',
-    icon: 'bi bi-people',
-    class: '',
-    extralink: false,
-    submenu: []
-  }
+ 
 ];
